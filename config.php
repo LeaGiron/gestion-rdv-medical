38331cade6274183c499ebb37b296dea9136006c
Léa Giron <giron.lea@outlook.fr>
2025-02-09 14:09:24 +0100

    Page de configuration de la base de données OK

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Configurations de la base de données
+$config = [
+    'host' => 'localhost',
+    'db' => 'gestion_de_rdv_medicaux',
+    'user' => 'administrateur',
+    'password' => 'RdVNAuDIYvMM1BppWx',
+    'charset' => 'utf8mb4'
+];
+?>
