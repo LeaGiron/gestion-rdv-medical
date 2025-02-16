4e278eeb2e475ce4482a919cd4cae1483f39c663
Léa Giron <giron.lea@outlook.fr>
2025-02-16 16:52:50 +0100

    Page d'inscription PHP corrigée

--- inscription.php
+++ inscription.php
@@ -16,17 +16,24 @@ try {
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
     $email = $_POST['email'];
-    $mot_de_passe = $_POST['password'];
+    $mot_de_passe = $_POST['mot_de_passe'];
+    $telephone = $_POST['telephone'];
+}
 
-    // Validation de l'email (format email)
+    // Vérifier que tous les champs sont remplis
+    if (empty($nom) || empty($prenom) || empty($email) || empty($telephone) || empty($mot_de_passe)) {
+        echo "Tous les champs sont obligatoires.";
+        exit;
+    }
+
+    // Validation de l'email
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "L'email n'est pas valide.";
         exit;
     }
-
-    // Hacher le mot de passe avant de l'insérer dans la base de données
-    $mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_DEFAULT);
 
     // Vérifier si l'email existe déjà dans la base de données
     $requete = $pdo->prepare("SELECT COUNT(*) FROM utilisateurs WHERE util_email = :email");
@@ -36,16 +43,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
+    // Hacher le mot de passe avant de l'insérer dans la base de données
+    $mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_DEFAULT);
+
     // Insérer les données dans la table utilisateurs
-    $requete = $pdo->prepare("INSERT INTO utilisateurs (util_email, util_mot_de_passe) VALUES (:email, :password)");
+    $requete = $pdo->prepare("INSERT INTO utilisateurs (util_nom, util_prenom, util_email, util_mot_de_passe, util_telephone) VALUES (:nom, :prenom, :email, :mot_de_passe, :telephone)");
     $requete->execute([
-        'email' => $email,
-        'password' => $mot_de_passe_hache
+        'nom' => $nom,
+            'prenom' => $prenom,
+            'email' => $email,
+            'mot_de_passe' => $mot_de_passe_hache,
+            'telephone' => $telephone
     ]);
 
     echo "Compte créé avec succès ! Vous pouvez maintenant vous connecter.";
     // Redirection vers la page de connexion
     header("Location: connexion.html");
     exit;
-}
+
 ?>
