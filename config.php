c49b86ed0c57426e4d45648e4167a6beb105bbda
Léa <133007284+LeaGiron@users.noreply.github.com>
2025-03-22 14:24:35 +0100

    Configuration vidée pour la sécurité (voir configuration sur le pdf)

--- config.php
+++ config.php
@@ -1,10 +1,1 @@
-<?php
-// Configurations de la base de données
-return [
-    'host' => 'localhost',
-    'db' => 'gestion_de_rdv_medicaux',
-    'user' => 'administrateur',
-    'password' => 'RdVNAuDIYvMM1BppWx',
-    'charset' => 'utf8mb4'
-];
-?>
+
