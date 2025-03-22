382b2bca5bef2fc689146163c67b59df12320fc6
Léa <133007284+LeaGiron@users.noreply.github.com>
2025-03-22 14:25:02 +0100

    Connexion à la bdd vidée pour la sécurité (voir configuration sur le pdf)

--- connexion-bdd.php
+++ connexion-bdd.php
@@ -1,14 +1,1 @@
-<?php
-// Récupérer les informations de connexion dans un autre fichier sécurisé
-$config = require 'config.php';
 
-$connexion = "mysql:host={$config['host']};dbname={$config['db']};charset={$config['charset']}";
-
-try {
-    $pdo = new PDO($connexion, $config['user'], $config['password']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    error_log("Erreur de connexion à la base de données : " . $e->getMessage());
-    die("Erreur de connexion. Contactez l'administrateur.");
-}
-?>
